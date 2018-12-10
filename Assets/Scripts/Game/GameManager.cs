08e9758fb8a4cee98f333fd72a151f265f32253e
DiegoTong <didres3217@gmail.com>
2018-12-09 22:54:36 -0500

    Added the damage animation

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -50,6 +50,7 @@ public class GameManager : MonoBehaviour
     public void OnEnemyEscape()
     {
         escapedEnemies++;
+        UIManager.Instance.ShowDamage();
         if (escapedEnemies == maxAllowedEscapedEnemies)
         {
             OnGameLose();
