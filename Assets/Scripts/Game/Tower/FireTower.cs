93e06e77d254f6e0b74758830e86664d40c3beb2
DiegoTong <didres3217@gmail.com>
2018-12-09 23:03:52 -0500

    Created the Fire Tower script and applied it to the fire tower, also fixed the particle effect thingy

---
+++ Assets/Scripts/Game/Tower/FireTower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTower : Tower
+{
+    public GameObject fireParticlesPrefab;
+
+    protected override void AttackEnemy()
+    {
+        base.AttackEnemy();
+
+        // Create new particles
+        GameObject particles = (GameObject)Instantiate(fireParticlesPrefab, transform.position + new Vector3(0, .5f),
+            fireParticlesPrefab.transform.rotation);
+        particles.transform.localScale *= aggroRadius / 10f; // Scale fire particle radius with the aggro radius
+
+        // Damage all enemies in range
+        foreach (Enemy enemy in EnemyManager.Instance.GetEnemiesInRange(transform.position, aggroRadius))
+        {
+            enemy.TakeDamage(attackPower);
+        }
+    }
+}
