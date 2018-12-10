df5eb637a25753fe4bb7e2f90be2937303dd2e00
DiegoTong <didres3217@gmail.com>
2018-12-09 19:16:59 -0500

    Created the UI Manager and set all the towerslots on the map

---
+++ Assets/Scripts/Game/UI/UIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+    public Transform enemyHealthBars;
+    public GameObject addTowerWindow;
+    public GameObject towerInfoWindow;
+    public GameObject winGameWindow;
+    public GameObject loseGameWindow;
+    public GameObject centerWindow;
+    public GameObject damageCanvas;
+    public GameObject blackBackground;
+    public Text txtGold;
+    public Text txtWave;
+    public Text txtEscapedEnemies;
+    public GameObject enemyHealthBarPrefab;
+    void Awake()
+    {
+        Instance = this;
+    }
+    private void UpdateTopBar()
+    {
+        txtGold.text = GameManager.Instance.gold.ToString();
+        txtWave.text = "Wave " + GameManager.Instance.waveNumber + " / " + WaveManager.Instance.enemyWaves.Count;
+        txtEscapedEnemies.text = "Escaped Enemies " + GameManager.Instance.escapedEnemies + " / " +
+                                 GameManager.Instance.maxAllowedEscapedEnemies;
+    }
+    public void ShowAddTowerWindow(GameObject towerSlot)
+    {
+        addTowerWindow.SetActive(true);
+        addTowerWindow.GetComponent<AddTowerWindow>().towerSlotToAddTowerTo = towerSlot;
+
+        UtilityMethods.MoveUiElementToWorldPosition(addTowerWindow.GetComponent<RectTransform>(), towerSlot.transform.position);
+    }
+    void Update()
+    {
+        UpdateTopBar();
+    }
+}
